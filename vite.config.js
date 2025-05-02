6ad9e38f240230433176c4afcbd0712dd729f897
Treafiddy <joshuafree0@gmail.com>
2025-05-02 10:34:57 +0100

    testing console log

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 const isDevelopment = process.env.NODE_ENV === "development";
-const padresURL = "https://joshuafree.github.io/Padre-Ginos"
+const padresURL = "https://joshuafree.github.io/Padre-Ginos";
+console.log(process.env.NODE_ENV);
 
 // https://vitejs.dev/config/
 export default defineConfig({
