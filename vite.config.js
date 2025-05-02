0a737f9d0b80750f3ac326f1513f49981865195e
Treafiddy <joshuafree0@gmail.com>
2025-05-02 10:29:25 +0100

    changing vite target

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,20 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import { TanstackRouterVite } from "@tanstack/router-plugin";
-
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:3000",
-//         changeOrigin: true,
-//       },
-//       "/public": {
-//         target: "http://localhost:3000",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-//   plugins: [TanstackRouterVite(), react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+const isDevelopment = process.env.NODE_ENV === "development";
+const padresURL = "https://joshuafree.github.io/Padre-Ginos"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: isDevelopment ? "http://localhost:3000" : padresURL,
         changeOrigin: true,
       },
       "/public": {
-        target: "http://localhost:3000",
+        target: isDevelopment ? "http://localhost:3000" : padresURL,
         changeOrigin: true,
       },
     },
